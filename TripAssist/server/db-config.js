a9a0f718ca5d13a9173036bf2e224f7d90382bb3
stheer <scotttheer@gmail.com>
2021-04-18 21:00:48 +0100

    Delete db-config.js

--- TripAssist/server/db-config.js
+++
@@ -1,9 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "cis550-proj.cws1vctyhuha.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "r6u37gnkieEW.nxg",
-  database: "Travel_App",
-  multipleStatements: true
-};
